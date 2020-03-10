ffaa1d11b18df3f7d57ab29b9e3148b4bdb78410
airily-note <it66@qq.com>
2020-03-10 17:06:43 +0800

    修改配置

--- dubbo-provider/src/main/java/com/favorv/dubbo/provider/ProviderApplication.java
+++ dubbo-provider/src/main/java/com/favorv/dubbo/provider/ProviderApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
+@SpringBootApplication
 @EnableDubboConfiguration
 @DubboComponentScan("com.favorv.dubbo.provider.common.impl")
 public class ProviderApplication {
